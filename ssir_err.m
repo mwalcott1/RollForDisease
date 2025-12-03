43257a4f178df9b8e962bda47619602fdc7869c5
Benw121 <151397453+Benw121@users.noreply.github.com>
2025-12-02 22:39:13 -0700

    Error across all three classes

--- ssir_err.m
+++ ssir_err.m
@@ -5,19 +5,21 @@ function err = ssir_err(tspan,y0,params,data)
 % there are many options to define the error, choose from below
 
 % Given specific parameter estimates, generate simulated data 
-us_pop = 331449281;
-y0(2) = data(1);
-y0(1) = us_pop-y0(2);
+pop = 5690000;
+y0(2) = 1;
+y0(1) = pop-y0(2);
 [t,y] = ode45(@(t,x) ssirODE(t,x,params),tspan,y0);
-y = y(:,2)/params(2);  % model solution for data comparison
 
 %% Specify your objective function to be minimized
 %  Note that you can apply ordinary least squares (OLS) using weights =  ones
 
-weights = ones(length(data),1);   % OLS
+% weights = ones(length(data),1);   % OLS
 % weights = 1./data;                % RWLS
 % weights = ones(length(data),1);   % GWLS
 % weights(2) = 0;
 % weights(3) = 0;
 % weights = 1./log(data); % Own
-err = sum((data-y).^2.*weights);  % objective function
+%err = sum((data-y).^2.*weights);  % objective function
+size(data);
+size(y);
+err = norm(data - y(:, 2:4), 2);
--- stochastic_SIR.m
+++ stochastic_SIR.m
@@ -2,11 +2,12 @@
 clear
 
 % load & process data
-path = "countries-aggregated.csv";
+path = "covid_singapore.csv";
 data = readmatrix(path);
-data = data(1:327, :);
+data = data(1:200, :);
+% data = data(1:327, :); % clean data if us
 dates = data(:, 1);
-infected = data(:, 2) - data(:, 3);
+infected = data(:, 2) - data(:, 3) - data(:,4);
 recovered = data(:, 3);
 deaths = data(:, 4);
 
@@ -17,7 +18,7 @@ plot(dates, infected)
 plot(dates, recovered)
 plot(dates, deaths)
 hold off
-legend('cases', 'recovered', 'deaths')
+legend('infected', 'recovered', 'deaths')
 
 %% Parameter estimation
 
@@ -32,17 +33,17 @@ params(2) = 1e-5; % delta
 % params(4) = 1e-5; % alpha
 
 %Initial conds (variables go in order: s, i, r, d)
-us_pop = 331449281;
+pop = 5690000;
 x0 = zeros(4,1);
-x0(2) = 1; % US patient zero
-x0(1) = us_pop; % us population
+x0(2) = 1; % patient zero
+x0(1) = pop; % us population
 x0(3) = 0; % no recovered yet
 x0(4) = 0; % no deaths yet
 
 % fminsearch (Nelder-Mead Simplex Method)
 % options = optimset('Display','iter');
 options = optimset('Display','iter','MaxFunEvals',10000, 'MaxIter',10000);
-best_params = fminsearch(@(p) ssir_err(dates,x0,p,infected),params,options);
+best_params = fminsearch(@(p) ssir_err(dates,x0,p,data(:, 2:4)),params,options);
 best_params
 
 %% Re-simulate the model with the final parameter estimates; 
