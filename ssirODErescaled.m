0bdc37e95c6d15f06e39478eca90933f917d510a
Benjamin <ben_wu@mines.edu>
2025-12-08 19:21:33 +0000

    rescaled ssir

---
+++ ssirODErescaled.m
@@ -0,0 +1,24 @@
+function dxdt = ssirODErescaled(t,x,params)
+%model equations for simulating the scaled SIWR model
+%x(1) = susceptibles s
+%x(2) = infecteds i
+%x(3) = recovered
+%x(4) = dead
+
+pop = 5690000;
+
+beta = params(1);
+gamma = params(2);
+% gamma = 0.1;
+delta = params(3);
+alpha = params(4);
+% alpha = 1/105; % do not estimate alpha
+
+N = 1 - x(4);
+
+dxdt = zeros(4,1); %column vector for the state variables
+dxdt(1) = -beta*x(1)*x(2)*N + alpha*x(3);
+dxdt(2) = beta*x(1)*x(2)*N - gamma*x(2) - delta*x(2);
+dxdt(3) = gamma*x(2) - alpha*x(3);
+dxdt(4) = delta*x(2);
+end
